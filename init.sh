c6b114eac64e44eb4baec963e597fdf44048452d
JOSÉ MANUEL HERNÁNDEZ BENÍTEZ <jm.hernandez@parlamentodeandalucia.es>
2018-05-14 09:23:13 +0200

    change port to 80

--- init.sh
+++ init.sh
@@ -4,4 +4,5 @@ set -e
 echo "Starting SSH ..."
 service ssh start
 
-python /code/manage.py runserver 0.0.0.0:8000
+python /code/manage.py runserver 0.0.0.0:80
+#python /code/manage.py runserver 0.0.0.0:8000
